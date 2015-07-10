b83da86a302f5ae05cf9ba3c090fb5aae26e72a5
runningship <runningship@gmail.com>
2015-07-11 00:25:43 +0800

    more

--- src/main/java/org/bc/sdak/SQL2008Dialect.java
+++ src/main/java/org/bc/sdak/SQL2008Dialect.java
@@ -5,6 +5,7 @@ import org.hibernate.dialect.SQLServer2008Dialect;
 public class SQL2008Dialect extends SQLServer2008Dialect{
 	public SQL2008Dialect() {  
         super();  
-        registerHibernateType(-16, "string");  
+        registerHibernateType(-16, "string");
+        registerHibernateType(-9, "string");
     }  
 }
