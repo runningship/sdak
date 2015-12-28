91190914584cfed146ce7743c218ccd82275c2f1
runningship <runningship@gmail.com>
2015-12-29 00:09:25 +0800

    多表

--- src/main/java/org/bc/sdak/SessionFactoryBuilder.java
+++ src/main/java/org/bc/sdak/SessionFactoryBuilder.java
@@ -8,43 +8,38 @@ import javax.persistence.Entity;
 import org.bc.sdak.utils.ClassUtil;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.NamingStrategy;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 public class SessionFactoryBuilder {
 
 	private static SessionFactory sf = null;
-	private static Configuration cfg = null;
+	private static Configuration cfg = new Configuration();
 	private static Map<String,String> settings = new HashMap<String,String>();
 	
-	public static void applySettings(Map<String,String> settings){
+	public static void applySettings(Map<String,String> settings , NamingStrategy namingStrategy){
 		SessionFactoryBuilder.settings = settings;
+		if(namingStrategy!=null){
+			cfg.setNamingStrategy(namingStrategy);
+		}
+	}
+	
+	public static void applySettings(Map<String,String> settings){
+		applySettings(settings , null);
 	}
 	
 	public synchronized static SessionFactory buildOrGet(){
 		if(sf == null){
-			cfg = new Configuration();
 			for(String key : settings.keySet()){
 				cfg.setProperty(key,settings.get(key));
 			}
+			
 			String packages = settings.get("annotated.packages");
 			if(packages!=null){
 				packages = packages.replace("\n", "");
 				addAnnotatedClass(packages.split(";"));
 			}
-//			cfg.setProperty(AvailableSettings.URL, "jdbc:mysql://localhost:3306/herb");
-//			cfg.setProperty(AvailableSettings.USER, "root");
-//			cfg.setProperty(AvailableSettings.PASS, "admin");
-//			cfg.setProperty(AvailableSettings.SHOW_SQL, "true");
-//			cfg.setProperty(AvailableSettings.DRIVER,"com.mysql.jdbc.Driver");
-//			cfg.setProperty(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-//			cfg.setProperty(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//			cfg.setProperty(AvailableSettings.HBM2DDL_AUTO, "update");
-			//cfg.configure();
-//			cfg.addAnnotatedClass(Herb.class);
-//			cfg.addAnnotatedClass(HerbProperty.class);
-//			cfg.addAnnotatedClass(Prescription.class);
-//			cfg.addAnnotatedClass(EffectIngredient.class);
 			ServiceRegistry registry = new ServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
 			sf = cfg.buildSessionFactory(registry);
 		}
